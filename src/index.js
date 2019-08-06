8126162e61f90d27e6d189e7a668b7a192e555af
Vivek Poola <vpoola88@gmail.com>
2019-08-05 17:22:34 -0700

    react scaffold for starter, including eslint & code prettier for vscode

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div>Hello React,Webpack 4 & Babel 7!</div>;
+
+ReactDOM.render(<App />, document.querySelector('#root'));
